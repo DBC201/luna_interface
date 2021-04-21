de10cd82359c6debf540fd42709d347b07bcf2a6
dbc201 <denizbcakiroglu@gmail.com>
2021-04-22 00:41:56 +0300

    bash scripts

---
+++ kill.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 11
+sudo_pid=$(ps aux | grep 'sudo node main.js' | awk '{if ($11 == "sudo"){print $2}}')
+sudo kill -9 "$sudo_pid"
+
+main_pid=$(ps aux | grep 'node main.js' | awk '{if ($11 == "node"){print $2}}')
+sudo kill -9 "$main_pid"
