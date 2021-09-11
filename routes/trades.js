69ff8a4572b065f0f3b2baced299d11af1c7519c
atillaturkmen <turkmenatilla522@gmail.com>
2021-09-11 15:20:16 +0300

    added gate trades

--- routes/trades.js
+++ routes/trades.js
@@ -3,6 +3,14 @@ require("dotenv").config({path: path.join(__dirname, "..", ".env.local")});
 const router = require("express").Router();
 const fs = require("fs");
 
+router.get("/trades/gate", function(req, res) {
+    let file_names = fs.readdirSync(process.env.gate_path);
+    res.render("files", {
+        type: "gate",
+        file_names: file_names
+    });
+});
+
 
 router.get("/trades/live", function(req, res) {
     let file_names = fs.readdirSync(process.env.live_path);
@@ -18,6 +26,21 @@ router.get("/trades/historical", function(req, res) {
         type: "historical",
         file_names: file_names
     });
+});
+
+router.get("/trades/gate/:file", function (req, res) {
+    let file_name = req.params.file;
+    let files = fs.readdirSync(process.env.gate_path);
+    if (files.indexOf(file_name) !== -1) {
+        res.render("graph", {
+            title: file_name.split('-')[0],
+            file_name: file_name,
+            data: fs.readFileSync(path.join(process.env.gate_path, file_name), "utf-8"),
+            type: "gate"
+        });
+    } else {
+        res.send("Wrong graph");
+    }
 });
 
 router.get("/trades/live/:file", function (req, res) {
