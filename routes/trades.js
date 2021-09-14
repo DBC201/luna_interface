2279c1ac040ab59f3669745d8bb63d381aaf9712
atillaturkmen <turkmenatilla522@gmail.com>
2021-09-14 14:59:38 +0300

    sort trade files

--- routes/trades.js
+++ routes/trades.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 
 router.get("/trades/gate", function(req, res) {
     let file_names = fs.readdirSync(process.env.gate_path);
+    file_names = sortGateFileNames(file_names);
     res.render("files", {
         type: "gate",
         file_names: file_names
@@ -14,6 +15,7 @@ router.get("/trades/gate", function(req, res) {
 
 router.get("/trades/live", function(req, res) {
     let file_names = fs.readdirSync(process.env.live_path);
+    file_names = sortBinanceFileNames(file_names);
     res.render("files", {
         type: "live",
         file_names: file_names
@@ -22,6 +24,7 @@ router.get("/trades/live", function(req, res) {
 
 router.get("/trades/historical", function(req, res) {
     let file_names = fs.readdirSync(process.env.historical_path);
+    file_names = sortBinanceFileNames(file_names);
     res.render("files", {
         type: "historical",
         file_names: file_names
@@ -72,5 +75,54 @@ router.get("/trades/historical/:file", function (req, res) {
         res.send("Wrong graph");
     }
 });
+
+function sortGateFileNames(file_names) {
+    return file_names.sort(function(a, b) {
+        let year1 = a.split("-")[1];
+        let year2 = b.split("-")[1];
+        if (year1 != year2) {
+            return year2 - year1;
+        }
+        let month1 = a.split("-")[2];
+        let month2 = b.split("-")[2];
+        if (month1 != month2) {
+            return month2 - month1;
+        }
+        let day1 = a.split("-")[3].substring(0, 2);
+        let day2 = b.split("-")[3].substring(0, 2);
+        if (day1 != day2) {
+            return day2 - day1;
+        }
+        let time1 = a.split("_")[2].split(".").join("").substring(0, 6);
+        let time2 = b.split("_")[2].split(".").join("").substring(0, 6);
+        return time2 - time1;
+    });
+}
+
+function sortBinanceFileNames(file_names) {
+    return file_names.sort(function(a, b) {
+        let date1 = a.split("_")[1];
+        let date2 = b.split("_")[1];
+
+        let year1 = date1.split("-")[0];
+        let year2 = date2.split("-")[0];
+        if (year1 != year2) {
+            return year2 - year1;
+        }
+        let month1 = date1.split("-")[1];
+        let month2 = date2.split("-")[1];
+        if (month1 != month2) {
+            return month2 - month1;
+        }
+        let day1 = date1.split("-")[2];
+        let day2 = date2.split("-")[2];
+        if (day1 != day2) {
+            return day2 - day1;
+        }
+        let time1 = a.split("_")[2].split(".").join("").substring(0, 6);
+        let time2 = b.split("_")[2].split(".").join("").substring(0, 6);
+        return time2 - time1;
+    });
+}
 
 module.exports = router;
